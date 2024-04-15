49c032e2e95240623a27f4c3be704844cdd0ab68
aakriti verma <aakritiv0709@gmail.com>
2024-04-15 21:42:41 +0530

    video tube project completed

---
+++ src/components/Videos.jsx
@@ -0,0 +1,13 @@
+import VideoCard from "./VideoCard";
+
+const Videos = ({ videos }) => {
+  return (
+    <div className="p-3 text-white flex flex-wrap items-center justify-around gap-y-1 sm:gap-y-3 md:gap-y-10">
+      {videos?.map((video, i) => (
+        <VideoCard key={i} video={video} />
+      ))}
+    </div>
+  );
+};
+
+export default Videos;
